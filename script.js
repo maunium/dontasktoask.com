07e06f2016eeae3676959d14ecaa2fdbe7e84c1c
Angelo II <62988428+AngeloCore@users.noreply.github.com>
2022-02-18 01:46:59 +0200

    Update

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ window.onload = () => {
       : navigator.language || navigator.userLanguage
   ).toLowerCase();
   const twoLettersLanguages = ["ar-eg", "pt-br", "pt-pt", "zh-cn", "zh-tw"];
-  const enLanguages = ["en-US", 'en-UK', 'en'];
+  const enLanguages = ["en-US", "en-UK", "en"];
 
-  if (enLanguages.includes(lang)) window.location.replace(window.location.hostname);
-  else if (twoLettersLanguages.includes(lang)) window.location.replace(`${window.location.hostname}/${lang}`);
-  else window.location.replace(`${window.location.hostname}/${lang.slice(0, 2)}`);
+  if (enLanguages.includes(lang))
+    window.location.replace(window.location.hostname);
+  else if (twoLettersLanguages.includes(lang))
+    window.location.replace(`${window.location.hostname}/${lang}`);
+  else
+    window.location.replace(`${window.location.hostname}/${lang.slice(0, 2)}`);
 };
